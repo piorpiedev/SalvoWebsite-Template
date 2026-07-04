2a8a1c7431af31ad37ee7608e20638254878ddd4
piorpiedev <contact@piorpie.com>
2026-07-04 12:54:16 +0200

    feat: add salvo-cli generated template

---
+++ src/db/mod.rs
@@ -0,0 +1,18 @@
+use std::sync::OnceLock;
+use sqlx::postgres::PgPool;
+
+use crate::config::DbConfig;
+
+pub static SQLX_POOL: OnceLock<PgPool> = OnceLock::new();
+
+pub async fn init(config: &DbConfig) {
+    let sqlx_pool = PgPool::connect(&config.url).await
+        .expect("Database connection failed.");
+    crate::db::SQLX_POOL
+        .set(sqlx_pool)
+        .expect("sqlx pool should be set")
+}
+
+pub fn pool() -> &'static PgPool {
+    SQLX_POOL.get().expect("sqlx pool should be set")
+}
